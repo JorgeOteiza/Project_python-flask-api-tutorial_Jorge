d77001ca558fa13fffce6d5d5362ceabb0a2751a
Jorge Oteiza <oteiza.jor@gmail.com>
2024-06-19 23:09:34 +0000

    ejercicio 08

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
-from flask import Flask # type: ignore
-from flask import Flask, jsonify # type: ignore
+from flask import Flask, jsonify, request # type: ignore
+
 app = Flask(__name__)
 
 
-# Variable global todos
+
+  # Variable global todos
 todos = [
     { "label": "My first task", "done": False },
     { "label": "My second task", "done": False }
@@ -14,7 +15,22 @@ def get_todos():
     return jsonify(todos)
 
 
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+
+
+    # Agregar el nuevo todo a la lista
+    todos.append(request_body)
+    
+    # Devolver la lista actualizada
+    return jsonify(todos)
+
+
 # Estas dos líneas siempre deben estar al final de tu archivo app.py
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
+
+
